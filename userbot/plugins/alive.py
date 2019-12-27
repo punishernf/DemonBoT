7952521a220f704a6151efb244081c9fd66657e6
DemonHuB <57061430+TheDemonHuB@users.noreply.github.com>
2019-12-27 22:26:48 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,4 +1,4 @@
-""".admin Plugin for @XtraTgBot"""
+""".admin Plugin for @thedemonhub"""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -11,7 +11,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ`**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @A_Dark_Princ3\n`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     f"`My peru owner`: {DEFAULTUSER}\n"
-                     "[Deploy this userbot Now](https://github.com/Dark-Princ3/X-tra-Telegram)")
+    await alive.edit("**`I AM ALIVE MASTER!!!!`**\n\n"
+                     "`My Great Developer: @demonhub\n`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
+                     f"`My Master`: {DEFAULTUSER}\n")
+                     
