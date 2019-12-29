06f35bca7c6e16e157bdf181dee31d5e58eaeed5
DemonHuB <57061430+TheDemonHuB@users.noreply.github.com>
2019-12-29 13:40:40 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -57,7 +57,7 @@ async def _(event):
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
         packname = f"{userid}'s KangAnimated"
-        if userid == 719877937:
+        if userid == 715462063:
             packshortname = "TheAnubis_Animated"
         else:
             packshortname = f"Kang_As{userid}" # format: Uni_Borg_userid
