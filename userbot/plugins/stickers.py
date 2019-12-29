6dba8a3a1d7f56bdc48e130f0b09b10544905dbe
DemonHuB <57061430+TheDemonHuB@users.noreply.github.com>
2019-12-29 14:24:51 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -58,7 +58,7 @@ async def _(event):
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
         packname = f"{userid}'s KangAnimated"
         if userid == 715462063:
-            packshortname = "715462063_DemonHuB"
+            packshortname = "DemonHuB"
         else:
             packshortname = f"Kang_As{userid}" # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
